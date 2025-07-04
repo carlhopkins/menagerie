21361e06f547f66e976feb078cbeb6f3002d1d3f
Carl Hopkins <mail@carlhopkins.com>
2025-07-04 19:35:52 +0100

    Create HyperV-Dep_example.ps1

---
+++ HyperV-Dep_example.ps1
@@ -0,0 +1,6 @@
+$partition = Read-Host -Prompt "Type in which partition would you like to create a virtual machine in"
+$VMname = Read-Host -Prompt "Type name of the server which will be created"
+New-Item "$partition\$VMname" -type directory
+Copy-Item -Path "\\NAS\location\test.vhdx" -Destination "$partition\$VMname"
+Rename-Item -Path "$partition\$VMname\test.vhdx" -NewName "$VMname.vhdx"
+New-VM -Name $VMname -MemoryStartupBytes 16GB -Generation 2 -VHDPath "$partition\$VMname\$VMname.vhdx"
